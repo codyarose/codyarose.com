c3452573f958326703aaa11fca7a335910fbbd31
codyarose <crose992@gmail.com>
2020-09-16 17:44:52 -0500

    Added breakpoints to theme object and refactored declaration file to fix theme object not maintaining types when writing css

--- styled.d.ts
+++ styled.d.ts
@@ -1,8 +1,8 @@
 import 'styled-components'
-import theme from './theme'
+import theme from './src/theme'
 
 type Theme = typeof theme
 
 declare module 'styled-components' {
-	export type DefaultTheme = Theme
+	export interface DefaultTheme extends Theme {}
 }
