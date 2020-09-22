d401d36c58f1a07991867f598ce0ced71c13f87f
codyarose <crose992@gmail.com>
2020-09-22 15:20:38 -0500

    Added responsive styles and moved styles to own file

---
+++ src/components/Projects/Projects.styled.ts
@@ -0,0 +1,167 @@
+import styled from 'styled-components'
+
+import { Text } from '../Text'
+
+const Container = styled.section`
+	background-color: ${({ theme }) => theme.colors.black};
+	color: ${({ theme }) => theme.colors.white};
+	${({ theme }) => theme.breakpoints.up('xl')} {
+		padding: ${({ theme }) => theme.spacing(28, 0)};
+	}
+	${({ theme }) => theme.breakpoints.down('lg')} {
+		padding: 11vw 0;
+	}
+	${({ theme }) => theme.breakpoints.down('md')} {
+		padding: ${({ theme }) => theme.spacing(17.5, 0)};
+	}
+`
+
+const Content = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: auto auto;
+	${({ theme }) => theme.breakpoints.down('sm')} {
+		grid-template-columns: 1fr;
+	}
+`
+
+const DescriptionContainer = styled.div`
+	position: relative;
+	width: 100%;
+	max-width: 560px;
+	justify-self: flex-end;
+	${({ theme }) => theme.breakpoints.up('xl')} {
+		padding: ${({ theme }) => theme.spacing(0, 8.75)};
+	}
+	${({ theme }) => theme.breakpoints.down('lg')} {
+		padding: 0 4vw;
+	}
+	${({ theme }) => theme.breakpoints.up('md')} {
+		grid-row: 2;
+		grid-column: 1;
+	}
+	${({ theme }) => theme.breakpoints.down('sm')} {
+		display: none;
+	}
+	& > * {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: inherit;
+	}
+`
+
+const Title = styled.div`
+	width: 100%;
+	max-width: 560px;
+	margin-bottom: 0;
+	${({ theme }) => theme.breakpoints.up('xl')} {
+		padding: ${({ theme }) => theme.spacing(0, 8.75)};
+	}
+	${({ theme }) => theme.breakpoints.down('lg')} {
+		padding: 0 4vw;
+	}
+	${({ theme }) => theme.breakpoints.up('md')} {
+		justify-self: flex-end;
+	}
+	${({ theme }) => theme.breakpoints.down('sm')} {
+		margin: 0 auto;
+		padding: 0 1.5rem;
+	}
+`
+
+const H2 = styled.h2`
+	font-size: 1rem;
+	font-family: ${({ theme }) => theme.fontFamily.sansSerif};
+	font-weight: 400;
+`
+
+const Description = styled(Text.Spaced)`
+	margin: 0;
+`
+
+const Projects = styled.div`
+	grid-row: 2;
+	${({ theme }) => theme.breakpoints.up('md')} {
+		margin-top: -3.5rem;
+	}
+	${({ theme }) => theme.breakpoints.down('sm')} {
+		margin: 0 auto;
+	}
+`
+
+const ProjectsList = styled.ul`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	font-size: 4.5rem;
+	letter-spacing: -4px;
+	font-family: ${({ theme }) => theme.fontFamily.serif};
+	margin: 0;
+	list-style-type: none;
+	${({ theme }) => theme.breakpoints.up('xl')} {
+		padding: ${({ theme }) => theme.spacing(0, 8.75)};
+	}
+	${({ theme }) => theme.breakpoints.down('lg')} {
+		font-size: 4rem;
+		padding: 0 4vw;
+	}
+	${({ theme }) => theme.breakpoints.down('md')} {
+		font-size: 3rem;
+	}
+	${({ theme }) => theme.breakpoints.down('sm')} {
+		align-items: center;
+		padding: 0 1.5rem;
+	}
+	${({ theme }) => theme.breakpoints.down(500)} {
+		font-size: 2.5rem;
+	}
+`
+
+interface ProjectItemProps {
+	active: boolean
+}
+const ProjectItem = styled.li<ProjectItemProps>`
+	opacity: ${({ active }) => (active ? 1 : 0.75)};
+	transition: opacity 0.2s ease-out;
+	${({ theme }) => theme.breakpoints.up('md')} {
+		--spacing: ${({ theme }) => theme.spacing(3.5)};
+		--default-showing: 10px;
+		position: relative;
+		padding-left: var(--spacing);
+		padding-right: var(--default-showing);
+		overflow: hidden;
+		&::before {
+			position: absolute;
+			content: '';
+			top: 50%;
+			left: 0;
+			transform: ${({ active }) =>
+				active
+					? `translate(0, -50%)`
+					: `translate(calc(-100% + var(--default-showing)), -50%)`};
+			width: 100%;
+			height: 1px;
+			background-color: currentColor;
+			transition: transform 0.5s cubic-bezier(0.42, 0, 0, 1);
+			pointer-events: none;
+		}
+	}
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+`
+
+export const Styled = {
+	Container,
+	Content,
+	DescriptionContainer,
+	Title,
+	H2,
+	Description,
+	Projects,
+	ProjectsList,
+	ProjectItem,
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,10 @@
 import React, { FC, useState, MouseEvent } from 'react'
-import styled from 'styled-components'
-import { FaGithub as GithubIcon } from 'react-icons/fa'
 
 import { Hr } from '../Hr'
 import { Text } from '../Text'
 import Fade from '../Fade'
 import { projects } from './data'
+import { Styled } from './Projects.styled'
 
 const Projects: FC = () => {
 	const [currentIndex, setCurrentIndex] = useState(0)
@@ -16,143 +15,46 @@ const Projects: FC = () => {
 	}
 
 	return (
-		<StyledContainer>
-			<StyledContent>
-				<StyledTitle>
-					<StyledH2>Projects</StyledH2>
+		<Styled.Container>
+			<Styled.Content>
+				<Styled.Title>
+					<Styled.H2>
+						<Text.Spaced>Projects</Text.Spaced>
+					</Styled.H2>
 					<Hr />
-				</StyledTitle>
-				<StyledDescriptionContainer>
+				</Styled.Title>
+				<Styled.DescriptionContainer>
 					{projects.map((project, i) => (
-						<StyledDescription key={i}>
+						<Styled.Description key={i}>
 							<Fade show={currentIndex === i}>
 								<p>{project.desc}</p>
-								<a
-									href={`https://github.com/codyarose/${project.githubSlug}`}
-									target="_blank"
-								>
-									<GithubIcon color="#fff" size="24" />
-								</a>
 							</Fade>
-						</StyledDescription>
+						</Styled.Description>
 					))}
-				</StyledDescriptionContainer>
-				<StyledProjects>
+				</Styled.DescriptionContainer>
+				<Styled.Projects>
 					<div />
-					<StyledProjectsList>
+					<Styled.ProjectsList>
 						{projects.map((project, i) => (
-							<StyledProjectItem
+							<Styled.ProjectItem
 								key={i}
 								onMouseEnter={handleHover}
 								value={i}
 								active={currentIndex === i}
 							>
-								{project.title}
-							</StyledProjectItem>
+								<a
+									href={`https://github.com/codyarose/${project.githubSlug}`}
+									target="_blank"
+								>
+									{project.title}
+								</a>
+							</Styled.ProjectItem>
 						))}
-					</StyledProjectsList>
-				</StyledProjects>
-			</StyledContent>
-		</StyledContainer>
+					</Styled.ProjectsList>
+				</Styled.Projects>
+			</Styled.Content>
+		</Styled.Container>
 	)
 }
 
 export default Projects
-
-const StyledContainer = styled.section`
-	background-color: ${({ theme }) => theme.colors.black};
-	color: ${({ theme }) => theme.colors.white};
-	${({ theme }) => theme.breakpoints.up('xl')} {
-		padding: ${({ theme }) => theme.spacing(28, 0)};
-	}
-	${({ theme }) => theme.breakpoints.down('lg')} {
-		padding: 11vw 0;
-	}
-`
-
-const StyledContent = styled.div`
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-template-rows: auto auto;
-`
-
-const StyledDescriptionContainer = styled.div`
-	position: relative;
-	width: 100%;
-	max-width: 560px;
-	padding: ${({ theme }) => theme.spacing(0, 8.75)};
-	justify-self: flex-end;
-	grid-row: 2;
-	grid-column: 1;
-	& > * {
-		position: absolute;
-		top: 0;
-		left: 0;
-		padding: inherit;
-	}
-`
-
-const StyledTitle = styled.div`
-	width: 100%;
-	max-width: 560px;
-	padding: ${({ theme }) => theme.spacing(0, 8.75)};
-	justify-self: flex-end;
-	margin-bottom: 0;
-`
-
-const StyledH2 = styled.h2`
-	text-transform: uppercase;
-	font-family: ${({ theme }) => theme.fontFamily.sansSerif};
-	font-weight: 400;
-`
-
-const StyledDescription = styled(Text.Spaced)`
-	margin: 0;
-`
-
-const StyledProjects = styled.div`
-	grid-row: 2;
-	margin-top: -3.5rem;
-`
-
-const StyledProjectsList = styled.ul`
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	padding: ${({ theme }) => theme.spacing(0, 8.75)};
-	font-size: 4.5rem;
-	letter-spacing: -4px;
-	font-family: ${({ theme }) => theme.fontFamily.serif};
-	margin: 0;
-	list-style-type: none;
-`
-
-interface ProjectItemProps {
-	active: boolean
-}
-const StyledProjectItem = styled.li<ProjectItemProps>`
-	--spacing: ${({ theme }) => theme.spacing(3.5)};
-	--default-showing: 10px;
-	position: relative;
-	padding-left: var(--spacing);
-	padding-right: var(--default-showing);
-	opacity: ${({ active }) => (active ? 1 : 0.75)};
-	cursor: default;
-	overflow: hidden;
-	transition: opacity 0.2s ease-out;
-	&::before {
-		position: absolute;
-		content: '';
-		top: 50%;
-		left: 0;
-		transform: ${({ active }) =>
-			active
-				? `translate(0, -50%)`
-				: `translate(calc(-100% + var(--default-showing)), -50%)`};
-		width: 100%;
-		height: 1px;
-		background-color: currentColor;
-		transition: transform 0.5s cubic-bezier(0.42, 0, 0, 1);
-	}
-`
