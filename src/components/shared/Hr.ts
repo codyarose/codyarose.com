563625065997a505516279043d0cdc245e5cdb42
codyarose <crose992@gmail.com>
2020-09-23 13:08:41 -0500

    Simplified Hr margin

--- src/components/shared/Hr.ts
+++ src/components/shared/Hr.ts
@@ -3,13 +3,5 @@ import styled from 'styled-components'
 export const Hr = styled.hr`
 	width: 100%;
 	background-color: currentColor;
-	${({ theme }) => theme.breakpoints.up('xl')} {
-		margin: 3.5rem 0;
-	}
-	${({ theme }) => theme.breakpoints.down('lg')} {
-		margin: 3vw 0;
-	}
-	${({ theme }) => theme.breakpoints.down('md')} {
-		margin: 2.5rem 0;
-	}
+	margin: ${({ theme }) => theme.spacing(2, 0)};
 `
