0176355e1dd7eebbcf7550b16c78a67c6f5e3c7b
codyarose <crose992@gmail.com>
2020-09-25 00:06:50 -0500

    Added BlogItem component

---
+++ src/components/Blog/BlogItem.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+interface Props {
+	title: string
+	slug: string
+	description: string
+	excerpt: string
+}
+
+const BlogItem: FC<Props> = ({ title, slug, description, excerpt }) => {
+	return (
+		<div>
+			<StyledLink to={`/blog${slug}`}>
+				<StyledTitle>{title || slug}</StyledTitle>
+				<StyledExcerpt
+					dangerouslySetInnerHTML={{ __html: description || excerpt }}
+				/>
+			</StyledLink>
+		</div>
+	)
+}
+
+export default BlogItem
+
+const StyledLink = styled(Link)`
+	display: block;
+	color: inherit;
+	text-decoration: none;
+`
+
+const StyledTitle = styled.h3`
+	position: relative;
+	display: inline-block;
+	font-size: 2.5rem;
+	letter-spacing: -0.06em;
+	margin-bottom: ${({ theme }) => theme.spacing(1)};
+	border-bottom: 1px solid currentColor;
+`
+
+const StyledExcerpt = styled.p`
+	line-height: 1.5;
+`
