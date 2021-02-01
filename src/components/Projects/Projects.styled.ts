3ed6b8348da8f9637039554b81ab0ed6958fa846
codyarose <crose992@gmail.com>
2021-01-31 22:54:01 -0600

    feat(component): :sparkles: add new and improved Projects section

--- src/components/Projects/Projects.styled.ts
+++ src/components/Projects/Projects.styled.ts
@@ -1,5 +1,76 @@
 import styled from 'styled-components'
 
-// const
+const Container = styled.section`
+	display: grid;
+	gap: 2rem;
+	ul {
+		list-style-type: none;
+	}
+`
 
-export const $ = {}
+const List = styled.ul`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+	gap: ${({ theme }) => theme.spacing(2)};
+	margin: 0;
+	${({ theme }) => theme.breakpoints.down('xs')} {
+		grid-template-columns: 1fr;
+	}
+`
+
+const Card = styled.li`
+	display: flex;
+	flex-direction: column;
+	padding: ${({ theme }) => theme.spacing(3)};
+	margin: 0;
+	background-color: hsl(0, 0%, 9%);
+	border-radius: 8px;
+	> p {
+		flex-grow: 1;
+	}
+	${({ theme }) => theme.breakpoints.down('xs')} {
+		padding: ${({ theme }) => theme.spacing(2)};
+	}
+`
+
+const Links = styled.ul`
+	margin: 1.25rem 0 0;
+	display: flex;
+	align-items: center;
+	text-transform: uppercase;
+	font-size: 0.75rem;
+	a {
+		display: flex;
+		svg {
+			margin-left: 0.5rem;
+		}
+	}
+	> a + a {
+		margin-left: 2rem;
+	}
+`
+
+const Tags = styled.ul`
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
+	font-size: 0.75rem;
+	margin: 1.25rem 0 0;
+	opacity: 0.5;
+	white-space: nowrap;
+	li {
+		border: 1px solid currentColor;
+		border-radius: 1rem;
+		padding: 0.25rem 0.5rem;
+		margin-right: 0.25rem;
+		margin-bottom: 0.25rem;
+	}
+`
+
+export const $ = {
+	Container,
+	List,
+	Card,
+	Links,
+	Tags,
+}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,16 +1,23 @@
-import React, { FC, useState, MouseEvent } from 'react'
+import React, { FC } from 'react'
 
 import { $ } from './Projects.styled'
+import { Text } from '../shared/Text'
 import { graphql, useStaticQuery } from 'gatsby'
+import Icon from '../shared/Icon'
 
 interface ProjectsQuery {
 	pageDataJson: {
 		home: {
 			projects: {
-				title: string
-				githubSlug: string
-				description: string
-			}[]
+				intro: string
+				list: {
+					title: string
+					githubUrl: string
+					description: string
+					demo: string
+					tags: string[]
+				}[]
+			}
 		}
 	}
 }
@@ -21,9 +28,14 @@ const Projects: FC = () => {
 			pageDataJson {
 				home {
 					projects {
-						title
-						githubSlug
-						description
+						intro
+						list {
+							title
+							githubUrl
+							description
+							demo
+							tags
+						}
 					}
 				}
 			}
@@ -33,7 +45,47 @@ const Projects: FC = () => {
 		home: { projects },
 	} = pageDataJson
 
-	return <div id="projects">projects</div>
+	return (
+		<$.Container>
+			<div>
+				<h2>Projects</h2>
+				<Text.Spaced as="p">{projects.intro}.</Text.Spaced>
+			</div>
+			<$.List id="projects">
+				{projects.list.map(project => (
+					<$.Card key={project.title}>
+						<h3>{project.title}</h3>
+						<Text.Spaced as="p">{project.description}</Text.Spaced>
+						<$.Links>
+							{project.githubUrl ? (
+								<a
+									href={project.githubUrl}
+									title={`${project.title} GitHub Repository`}
+									target="_blank"
+								>
+									Code
+									<Icon variant="chevronRight" width="6" />
+								</a>
+							) : null}
+							{project.demo ? (
+								<a href={project.demo} target="_blank">
+									Demo
+									<Icon variant="chevronRight" width="6" />
+								</a>
+							) : null}
+						</$.Links>
+						{project.tags ? (
+							<$.Tags>
+								{project.tags.map(tag => (
+									<li>{tag}</li>
+								))}
+							</$.Tags>
+						) : null}
+					</$.Card>
+				))}
+			</$.List>
+		</$.Container>
+	)
 }
 
 export default Projects
