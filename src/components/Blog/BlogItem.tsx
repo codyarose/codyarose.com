d045260ec37ad4f387a3005a1cc7b3cb97a2fc2e
codyarose <crose992@gmail.com>
2021-02-01 01:32:08 -0600

    refactor(styles): :recycle: refactor blog post styles, replaced bio section with Contact

--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import Icon from '../shared/Icon'
 
 interface Props {
 	title: string
@@ -12,34 +13,50 @@ interface Props {
 const BlogItem: FC<Props> = ({ title, slug, description, excerpt }) => {
 	return (
 		<div>
-			<StyledLink to={`/blog${slug}`}>
-				<StyledTitle>{title || slug}</StyledTitle>
-				<StyledExcerpt
-					dangerouslySetInnerHTML={{ __html: description || excerpt }}
-				/>
-			</StyledLink>
+			<$Title>
+				<Link to={`/blog${slug}`}>{title || slug}</Link>
+			</$Title>
+			<$Excerpt
+				dangerouslySetInnerHTML={{ __html: description || excerpt }}
+			/>
+			<$ReadMoreLink to={`/blog${slug}`}>
+				Read more
+				<Icon variant="chevronRight" width="4" />
+			</$ReadMoreLink>
 		</div>
 	)
 }
 
 export default BlogItem
 
-const StyledLink = styled(Link)`
-	display: block;
-	color: inherit;
-`
-
-const StyledTitle = styled.h3`
+const $Title = styled.h3`
 	position: relative;
 	display: inline-block;
 	font-size: 2.5rem;
 	letter-spacing: -0.06em;
 	margin-bottom: ${({ theme }) => theme.spacing(1)};
+	opacity: 0.8;
+	transition: opacity 0.2s ease;
 	${({ theme }) => theme.breakpoints.down('xs')} {
 		font-size: 2rem;
 	}
+	&:hover {
+		opacity: 1;
+	}
 `
 
-const StyledExcerpt = styled.p`
+const $Excerpt = styled.p`
 	line-height: 1.5;
+`
+
+const $ReadMoreLink = styled(Link)`
+	display: flex;
+	align-items: center;
+	&:hover svg {
+		transform: translateX(3px);
+	}
+	svg {
+		margin-left: 0.5rem;
+		transition: transform 0.2s ease;
+	}
 `
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,12 +4,13 @@ import Img from 'gatsby-image'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import styled from 'styled-components'
 
-import Bio from '../components/bio'
+// import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Container } from '../components/shared/Container'
 import { Content } from '../components/shared/Content'
 import { Hr } from '../components/shared/Hr'
+import Contact from '../components/Contact'
 
 interface Props {
 	data: {
@@ -44,52 +45,54 @@ const BlogPostTemplate: FC<Props> = ({ data, location }) => {
 				description={description || post.excerpt}
 				image={featuredImage.childImageSharp.fluid.src}
 			/>
-			<Container compact={true}>
-				<StyledContent compact={true}>
-					<StyledTitle>{title}</StyledTitle>
-					<StyledDate>{date}</StyledDate>
-					<StyledImgWrapper>
-						<Img fluid={featuredImage.childImageSharp.fluid} />
-					</StyledImgWrapper>
-					<StyledBody>
-						<MDXRenderer>{post.body}</MDXRenderer>
-					</StyledBody>
-					<StyledHr />
-					<Bio />
-				</StyledContent>
-			</Container>
+			<$Content>
+				<$Header>
+					<h1>{title}</h1>
+					<$Date>{date}</$Date>
+				</$Header>
+				<div>
+					<Img fluid={featuredImage.childImageSharp.fluid} />
+				</div>
+				<$Body>
+					<MDXRenderer>{post.body}</MDXRenderer>
+				</$Body>
+				<Hr />
+				<Contact />
+			</$Content>
 		</Layout>
 	)
 }
 
 export default BlogPostTemplate
 
-const StyledContent = styled(Content)`
-	font-size: 18px;
+const $Content = styled.div`
+	display: grid;
+	width: 100%;
+	grid-auto-rows: auto;
+	grid-template-columns: minmax(0, 1fr);
+	gap: 4rem;
 	line-height: 1.7;
+	padding: 5rem 0;
+	${({ theme }) => theme.breakpoints.up('sm')} {
+		font-size: 18px;
+	}
+	${({ theme }) => theme.breakpoints.down('xs')} {
+		padding: 2rem 0;
+		gap: 2rem;
+	}
 `
 
-const StyledTitle = styled.h1`
+const $Header = styled.div`
 	text-align: center;
 `
 
-const StyledDate = styled.p`
+const $Date = styled.p`
 	text-transform: uppercase;
-	text-align: center;
 	letter-spacing: 4px;
 	font-size: 0.75rem;
 `
 
-const StyledImgWrapper = styled.div`
-	padding-top: ${({ theme }) => theme.spacing(2)};
-`
-
-const StyledBody = styled.div`
-	padding-top: ${({ theme }) => theme.spacing(3)};
-	${({ theme }) => theme.breakpoints.down('xs')} {
-		padding-top: ${({ theme }) => theme.spacing(2)};
-	}
-
+const $Body = styled.div`
 	a {
 		color: ${({ theme }) => theme.colors.accent};
 	}
@@ -100,10 +103,12 @@ const StyledBody = styled.div`
 	h2 {
 		font-size: 1.5em;
 	}
-`
-
-const StyledHr = styled(Hr)`
-	margin: ${({ theme }) => theme.spacing(4, 0)};
+	pre {
+		margin: 3rem 0;
+		${({ theme }) => theme.breakpoints.down('xs')} {
+			margin: 2rem 0;
+		}
+	}
 `
 
 export const pageQuery = graphql`
